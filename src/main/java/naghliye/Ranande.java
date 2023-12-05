eee87246f534c9ee6d5db75680dcbfca451a0514
E.Motahari <e.motahari@gmail.com>
2023-12-05 10:32:56 +0330

    commit and Push

--- src/main/java/naghliye/Ranande.java
+++ src/main/java/naghliye/Ranande.java
@@ -1,4 +1,26 @@
 package naghliye;
 
-class Ranande {
+public class Ranande {
+    private String noeGavahiname = "Paye 1";
+    private static String nam = "Farhad";
+
+    //Non-static Nested Class : Inner Class
+    public class    Gavahiname {
+        public void  print(){
+            System.out.println("Class Non Static");
+            System.out.println(noeGavahiname);
+            System.out.println(nam);
+        }
+    }
+
+
+    //static Nested Class
+    public static class    StaticGavahiname {
+        public void  print(){
+            System.out.println("Class Non Static");
+//            System.out.println(noeGavahiname);        چون استتیک نیست نستت کلاس استتیک نمیتواند استفاده کند چون
+//            استتیک ها فقط یکبار و در زمان کامپایل تایم درست میشود و بقیه اجزا در زمان ران تایم ساخته میشوند
+            System.out.println(nam);
+        }
+    }
 }
